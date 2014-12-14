e41c610be2c8707a8e6fc1fe241b7f9c78c760f1
Scott Nelson <scott@scttnlsn.com>
2014-12-14 13:51:33 -0500

    Added comment toggling

--- modules/scttnlsn-editor.el
+++ modules/scttnlsn-editor.el
@@ -28,4 +28,17 @@
 
 ;; set cursor to vertical bar
 (set-default 'cursor-type 'bar)
+
+;; comments
+(defun comment-or-uncomment-region-or-line ()
+  "Comments or uncomments the region or the current line if there's no active region."
+  (interactive)
+  (let (beg end)
+    (if (region-active-p)
+        (setq beg (region-beginning) end (region-end))
+      (setq beg (line-beginning-position) end (line-end-position)))
+    (comment-or-uncomment-region beg end)))
+
+(global-set-key (kbd "C-x /") 'comment-or-uncomment-region-or-line)
+
 (provide 'scttnlsn-editor)
