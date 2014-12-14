8936ac3d116bba62a79c7d7fbfc0cacd9346fb07
Scott Nelson <scott@scttnlsn.com>
2014-12-14 13:51:46 -0500

    Use regexp search functions by default

--- modules/scttnlsn-editor.el
+++ modules/scttnlsn-editor.el
@@ -41,4 +41,10 @@
 
 (global-set-key (kbd "C-x /") 'comment-or-uncomment-region-or-line)
 
+;; search
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
 (provide 'scttnlsn-editor)
