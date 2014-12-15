83f31ede3d4e2fe05d0729e3cf90a228c3d85b0d
Scott Nelson <scott@scttnlsn.com>
2014-12-15 12:22:44 -0500

    Disable conflicting keybinding (code folding in web-mode)

--- modules/scttnlsn-web.el
+++ modules/scttnlsn-web.el
@@ -4,4 +4,10 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (setq web-mode-markup-indent-offset 2)
 
+(setq css-indent-offset 2)
+(setq less-css-indent-level 2)
+
+(add-hook 'web-mode-hook (lambda ()
+                           (define-key web-mode-map (kbd "C-c C-f") nil)))
+
 (provide 'scttnlsn-web)
