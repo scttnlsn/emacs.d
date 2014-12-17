5683fc07078bd3fdc97b8702cf8c20dca3ae5078
Scott Nelson <scott@scttnlsn.com>
2014-12-17 15:19:07 -0500

    Use web-mode for JavaScript

--- modules/scttnlsn-web.el
+++ modules/scttnlsn-web.el
@@ -3,9 +3,11 @@
 
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 
 (setq web-mode-markup-indent-offset 2)
 
+(setq web-mode-code-indent-offset 2)
 (setq css-indent-offset 2)
 (setq less-css-indent-level 2)
 
