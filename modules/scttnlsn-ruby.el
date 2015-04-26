a92f0ed738a17131f699fc3d70f7463bcc71fa43
Scott Nelson <scott@scttnlsn.com>
2015-04-26 09:41:33 -0400

    Added robe and bundler

--- modules/scttnlsn-ruby.el
+++ modules/scttnlsn-ruby.el
@@ -1,6 +1,15 @@
 (package-require 'rspec-mode)
+(package-require 'robe)
+(package-require 'bundler)
+
+;; robe
+(add-hook 'ruby-mode-hook 'robe-mode)
+(eval-after-load 'company
+  '(push 'company-robe company-backends))
 
 ;; don't use rake to run rspec
 (setq rspec-use-rake-flag nil)
+
+(setq ruby-deep-indent-paren nil)
 
 (provide 'scttnlsn-ruby)
