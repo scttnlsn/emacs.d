915439775f348765b80c1d86f810f3be0f41c4e2
Scott Nelson <scott@scttnlsn.com>
2015-05-26 15:16:34 -0400

    Update git config

--- modules/scttnlsn-git.el
+++ modules/scttnlsn-git.el
@@ -1,5 +1,8 @@
 (package-require 'magit)
 
+(setq magit-auto-revert-mode nil)
+(setq magit-last-seen-setup-instructions "1.4.0")
+
 (global-set-key (kbd "C-x g") 'magit-status)
 
 (provide 'scttnlsn-git)
