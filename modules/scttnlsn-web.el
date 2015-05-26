70b0fc7ba833ef79dea61c06bb8c12016b1d2f1c
Scott Nelson <scott@scttnlsn.com>
2015-05-26 15:16:50 -0400

    Don't use web mode for JS

--- modules/scttnlsn-web.el
+++ modules/scttnlsn-web.el
@@ -5,7 +5,6 @@
 
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 
 (setq web-mode-markup-indent-offset 2)
 
